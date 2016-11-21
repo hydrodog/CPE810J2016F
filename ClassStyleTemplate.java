54d8eed136bb583ad2a66af040d75985fb1288be
Dov Kruger <dov.kruger@gmail.com>
2016-11-20 23:10:27 -0500

    code style example

---
+++ ClassStyleTemplate.java
@@ -0,0 +1,50 @@
+/**
+	 @author: Dov Kruger, name, name
+	 @modified: Dov .....
+
+   Explain your section of the project once
+
+	 The calendar for assignments. The purpose is to make it easier for
+	 teachers to assign without entering dates, having to re-enter dates
+	 when events happen and classes are cancelled
+
+ */
+
+
+
+/**
+	 Explain what the class is for
+ */
+public class StyleTemplate { // class Names use camelcase with first letter capital
+   	private int privateData;
+	  private static int count;
+
+	  public static int getCount();
+	/*
+		Explain what the method does.  Hopefully in one short sentence, IF NECESSARY, more.
+
+	 */
+
+   //TODO: DK .... 
+
+
+   	public void f() {
+			  c++; // add 1 to c :  Don't do this!  Explain WHY you are adding 1 to c
+        if (   ) {
+
+				}
+				if (   ) {
+
+
+				} else {
+
+
+				}
+
+
+		}
+	  public void thisIsaLongMethodName(); // use camel case for method names
+
+   	public int getPrivateData() { return privateData; }
+	  public void setPrivateData(int val) { privateData = val; }
+}
